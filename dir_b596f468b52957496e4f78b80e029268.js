62a3041af3a0f3cc338d492c55f5f83788fbfacb
Cadre1 <cadre200@gmail.com>
2024-05-22 02:15:54 -0700

    Fixing Doxygen

--- dir_b596f468b52957496e4f78b80e029268.js
+++ dir_b596f468b52957496e4f78b80e029268.js
@@ -1,6 +1,7 @@
 var dir_b596f468b52957496e4f78b80e029268 =
 [
     [ "main.c", "main_8c.html", "main_8c" ],
+    [ "motor_driver.c", "motor__driver_8c.html", null ],
     [ "stm32f4xx_hal_msp.c", "stm32f4xx__hal__msp_8c.html", "stm32f4xx__hal__msp_8c" ],
     [ "stm32f4xx_it.c", "stm32f4xx__it_8c.html", "stm32f4xx__it_8c" ],
     [ "syscalls.c", "syscalls_8c.html", null ],
